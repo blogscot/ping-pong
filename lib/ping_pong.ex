f9ddbd1e1684f022bd61fcd244153126218540b1
blogscot <iain.diamond@gmail.com>
2017-03-12 15:04:31 +0000

    Add compression logic

--- lib/ping_pong.ex
+++ lib/ping_pong.ex
@@ -13,16 +13,22 @@ defmodule PingPong do
     iex --name node2@127.0.0.1 --cookie fudge -S mix
   """
 
-  @payload_length 1
-  @max_count 10_000
+  @max_count 10_000        # number of PingPong messages
+  @compression_level 1
 
   @doc """
-  When a :ping message is received, return a :pong
+  When a :ping message is received return a :pong
   """
   def ping do
     receive do
       {from, :ping, payload} ->
         send from, {self(), :pong, payload}
+      {from, :pingc, compressed_payload} ->
+        # unpack the compressed payload
+        payload = compressed_payload |> :erlang.binary_to_term([:safe])
+        # processing complete, so re-compress
+        new_payload = payload |> :erlang.term_to_binary([{:compressed, @compression_level}])
+        send from, {self(), :pongc, new_payload}
     end
     ping()
   end
@@ -35,6 +41,12 @@ defmodule PingPong do
     receive do
       {from, :pong, payload} ->
         send from, {self(), :ping, payload}
+      {from, :pongc, compressed_payload} ->
+        # unpack the compressed payload
+        uncompressed_payload = compressed_payload |> :erlang.binary_to_term([:safe])
+        # processing complete, so re-compress
+        new_payload = uncompressed_payload |> :erlang.term_to_binary([{:compressed, @compression_level}])
+        send from, {self(), :pingc, new_payload}
     end
 
     if (count < @max_count) do
@@ -48,14 +60,21 @@ defmodule PingPong do
   @doc """
   Spawn local and remote ping pong processes.
   """
-  def start_processes(nodes, payload) do
+  def start_processes(nodes, payload, compress? \\ false) do
 
     local  = List.first(nodes)
     remote = Enum.at(nodes, 1)
 
     pong_pid = Node.spawn_link local,  __MODULE__, :pong, [self()]
     ping_pid = Node.spawn_link remote, __MODULE__, :ping, []
-    send pong_pid, {ping_pid, :pong, payload}
+
+    case compress? do
+      true ->
+        new_payload = payload |> :erlang.term_to_binary([{:compressed, @compression_level}])
+        send pong_pid, {ping_pid, :pongc, new_payload}
+      _ ->
+        send pong_pid, {ping_pid, :pong, payload}
+    end
 
     receive do
       {^pong_pid, :done} -> IO.puts "Done!"
@@ -67,7 +86,7 @@ defmodule PingPong do
   and sends the initial message to start.
   If there is a connection problem, we give up and go home.
   """
-  def run do
+  def run(payload_length \\ 1, compress? \\ false) do
 
     # Read remote node info from config
     nodes = Application.get_env(:ping_pong, :nodes)
@@ -78,8 +97,8 @@ defmodule PingPong do
     # Are all nodes connected?
     case Enum.all?(status, &(&1)) do
       true ->
-        payload = 1..@payload_length |> Enum.to_list
-        Measure.this(fn -> start_processes(nodes, payload) end)
+        payload = 1..payload_length |> Enum.to_list
+        Measure.this(fn -> start_processes(nodes, payload, compress?) end)
         |> Kernel./(@max_count)
       _ ->
         IO.puts "Could not connect to remote nodes, status: #{inspect status}"
